ece3330a81be59664189bc6dd348ec9cf3aec96c
Yuyuan Chan <yuyuan17@live.com.my>
2021-05-14 11:38:41 -0700

    Update Reminder.ps1
    Fixed an issue where the date comparison between ScheduledDate vs today's date is incorrectly handled

--- Reminder.ps1
+++ Reminder.ps1
@@ -12,7 +12,7 @@ function IsHoliday([DateTime]$InputDate) {
         '11/26/2021', # Black Friday
         '12/23/2021', # Christmas Eve
         '12/24/2021'  # Christmas
-    $InputDateString = $InputDate.ToString('MM/dd/yyyy')
+    $InputDateString = $InputDate.ToShortDateString()
     foreach ($Holiday in $Holidays) {
         if ($Holiday -eq $InputDateString) {
             return $true
@@ -45,15 +45,15 @@ Example Scenario
 Powershell should show a popup message on 15th and the last day of every month, only if it is a business day.
 If not, Powershell should show a popup message on the previous business day.
 #>
-$InputDate = Get-Date '05/31/2021' # Get today's date
+$InputDate = Get-Date # Get today's date
 $TargetDay = 15 # By default, we set the target day to the 15th
 if ($InputDate.Day -gt 15) { # If today's date is after the 15th
     $TargetDay = [DateTime]::DaysInMonth($InputDate.Year, $InputDate.Month) # Set target day to the last day of the month
 }
 $TargetDate = Get-Date "$($InputDate.Month)/$($TargetDay)/$($InputDate.Year)" # Construct the target date to be in the format of MM/DD/YYYY
 $ScheduledDate = FindPreviousBusinessDay -InputDate $TargetDate # Find the pevious business day
-if ($ScheduledDate -eq $InputDate) { # If today is the scheduled day
+if ($ScheduledDate.ToShortDateString() -eq $InputDate.ToShortDateString()) { # If today is the scheduled day
     $Wshell = New-Object -ComObject Wscript.Shell # Creating a shell object
-    $Subject = "Your task is due on $($ScheduledDate.ToString('MM/dd/yyyy')) (today)" # Setting the message
+    $Subject = "Your task is due on $($ScheduledDate.ToShortDateString()) (today)" # Setting the message
     $Wshell.Popup("$Subject", 600, "Reminder", 64) # Show the popup
 }
